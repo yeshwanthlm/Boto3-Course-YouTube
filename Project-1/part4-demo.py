8c1d96dd41f06a2cad97f1bad92e7fc33a18614f
yeshwanthlm <yeshwanthpatel99@gmail.com>
2022-10-18 00:32:25 +0530

    chnages made

--- Project-1/part4-demo.py
+++ Project-1/part4-demo.py
@@ -2,7 +2,7 @@ import boto3
 
 aws_management_console = boto3.session.Session(profile_name="default")
 iam_console_resource = aws_management_console.resource('iam') # Resource Object
-iam_console_client = aws_management_console.resource('iam') # Client Object
+iam_console_client = aws_management_console.client('iam') # Client Object
 
 # Listiing iam users with resource object:
 for each_user in iam_console_resource.users.all():
@@ -11,3 +11,6 @@ for each_user in iam_console_resource.users.all():
 # Listing iam users with client object:
 for each in iam_console_client.list_users()['Users']:
    print(each['UserName'])
+
+#dir(aws_management_console)
+#print(aws_management_console.get_available_resources())
