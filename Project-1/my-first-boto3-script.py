1ac8e2b94e2a6b6b23ce387d0e22c482ec158d74
yeshwanthlm <yeshwanthpatel99@gmail.com>
2022-10-17 17:24:19 +0530

    First Project Commit

---
+++ Project-1/my-first-boto3-script.py
@@ -0,0 +1,7 @@
+import boto3
+
+aws_management_console=boto3.session.Session(profile_name="default")
+iam_console=aws_management_console.resource('iam')
+
+for each_user in iam_console.users.all():
+    print(each_user.name)
